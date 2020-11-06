03c5d27577d8eb869d0e8fdb0a4b926c66ed53e2
Lucas Boisbourdin <lucas.boisbourdin@epitech.eu>
2020-11-06 15:31:23 +0100

    Disable host check

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 module.exports = {
   // options...
   devServer:{
+    disableHostCheck: true,
       https: {
         key: fs.readFileSync('./cert/selfsigned_key.pem'),
         cert: fs.readFileSync('./cert/selfsigned.pem')
